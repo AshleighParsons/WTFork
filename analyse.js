9c858efcf6554fa8171765c5b3d26677470d0a14
Ashleigh Parsons <ashleigh@teamgeek.io>
2019-10-20 12:06:04 +0200

    link to wolfram, display nutritional info

--- analyse.js
+++ analyse.js
@@ -4,7 +4,7 @@ var myWolframAppId = 'RWU4HE-Q9UPPVXHQU';
 var app = new Clarifai.App({apiKey: myClarifaiApiKey});
 
 function loadFood(value, source) {
-  var preview = $(".food-photo");
+  var preview = $(".foodPic");
   var file    = document.querySelector("input[type=file]").files[0];
   var loader  = "https://media.giphy.com/media/5UG0A0ZV8APqnWYU0t/giphy.gif";
   var reader  = new FileReader();
@@ -19,7 +19,7 @@ function loadFood(value, source) {
 
   if (file) {
     reader.readAsDataURL(file);
-    $('#concepts').html('<img src="' + loader + '" class="loading" />');
+    $('#foodInfo').html('<img src="' + loader + '" class="loading" />');
   } else { 
     alert("No file selcted!"); 
   }
@@ -30,7 +30,14 @@ function analyseFood(value) {
   app.models.predict(Clarifai.FOOD_MODEL, value).then(function(response) {
       if(response.rawData.outputs[0].data.hasOwnProperty("concepts")) {
         var tag = response.rawData.outputs[0].data.concepts[0].name;
-        $('#concepts').html('<h3>'+ tag + '</h3>');
+
+        // link to Wolfram
+        var url = 'http://api.wolframalpha.com/v2/query?input='+tag+'%20nutrition%20facts&appid='+myWolframAppId;
+
+        // post nutritional info
+        getNutritionalInfo(url, function (result) {
+          $('#foodInfo').html('<h3>'+ tag + '</h3>' + "<img src='"+result+"'>");
+        });
       }
     }, function(err) { console.log(err); }
   );
